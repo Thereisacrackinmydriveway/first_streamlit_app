9c3ff8e073f90328b63afbc70291f450fe93a2fe
Thereisacrackinmydriveway <143822119+Thereisacrackinmydriveway@users.noreply.github.com>
2023-09-01 10:19:25 -0500

    Update streamlit_app.py
    Narrow list based on selections

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,10 +18,11 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # Set dropdown values for multiselect
 my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected =streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # display the table on the page
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
 
 
 
