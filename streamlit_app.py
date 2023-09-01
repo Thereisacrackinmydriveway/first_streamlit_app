175677feb46f1c462d210b2b01ed287df9f69402
Thereisacrackinmydriveway <143822119+Thereisacrackinmydriveway@users.noreply.github.com>
2023-09-01 10:13:06 -0500

    Update streamlit_app.py
    typo fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 ## Let's put a pick list here so they can pick the fruit they want to include
 
 # Set dropdown values for multiselect
-my_fruit_list = my_fruit_list.set_index('Fruit")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # display the table on the page
