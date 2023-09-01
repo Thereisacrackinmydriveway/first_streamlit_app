c275e23deaa4271bfec5b854449f7a3b842d2c5a
Thereisacrackinmydriveway <143822119+Thereisacrackinmydriveway@users.noreply.github.com>
2023-09-01 09:59:46 -0500

    Update streamlit_app.py
    Fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import streamlit
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
-strealit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
